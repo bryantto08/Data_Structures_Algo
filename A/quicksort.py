891b2dab47e91734b608f2839d7cdb573bba8ec7
Bryant To <bryantto08@gmail.com>
2022-08-10 12:39:09 -0400

    quicksort added

--- A/quicksort.py
+++ A/quicksort.py
@@ -1,9 +1,10 @@
 # Quicksort Algorithm
+# Quicksort: Using a Pivot to separate elements less than pivot from elements greater than pivot
 
 def quicksort(array):
-    if len(array) < 2:
+    if len(array) < 2:  # Base Case: If length of array equals 1 or 0
         return array
-    pivot = array[0]
+    pivot = array[0]  # Pivot used
     less = [i for i in array[1:] if i <= pivot]
     greater = [i for i in array[1:] if i > pivot]
-    return quicksort(less) + [pivot] + quicksort(greater)
+    return quicksort(less) + [pivot] + quicksort(greater)  # Then recursively calling it until it reaches base case
